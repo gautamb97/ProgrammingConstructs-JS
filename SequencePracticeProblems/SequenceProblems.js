eeb1cf144fca73e4fcd5e844dd9db008ef3001f0
gautamb97 <www.gautam971997@gmail.com>
2021-03-23 20:21:17 +0530

    Added:Addtion of two different dice values

--- SequencePracticeProblems/SequenceProblems.js
+++ SequencePracticeProblems/SequenceProblems.js
@@ -5,3 +5,11 @@ console.log(singleRandomValue);
 //generating dice values between 1-6
 let diceRandomValue = 1 + (Math.floor(Math.random() * 10) % 6);
 console.log(diceRandomValue);
+
+//adding two dice values
+let value = 0;
+for(let times = 0; times < 2; times++){
+    let diceRandomValue = 1 + (Math.floor(Math.random() * 10) % 6);
+    value += diceRandomValue;
+}
+console.log("Sum of two dice values: "+value);
