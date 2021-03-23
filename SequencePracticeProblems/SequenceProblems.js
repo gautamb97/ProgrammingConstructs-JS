149c9aa29c062175ab67b43a9168b767ba82642c
gautamb97 <www.gautam971997@gmail.com>
2021-03-23 20:38:26 +0530

    Added:Found sum and avg of five dice values

--- SequencePracticeProblems/SequenceProblems.js
+++ SequencePracticeProblems/SequenceProblems.js
@@ -13,3 +13,13 @@ for(let times = 0; times < 2; times++){
     value += diceRandomValue;
 }
 console.log("Sum of two dice values: "+value);
+
+//Write a program that reads 5 Random 2 Digit values , then find their sum and the average
+let values = 0;
+for(let numOftimes = 0; numOftimes < 5; numOftimes++){
+    let diceRandomValue = Math.floor(Math.random() * 90 + 10);
+    console.log(diceRandomValue);
+    values += diceRandomValue;
+}
+console.log("Sum of five dice values: "+values);
+console.log("Average of five dice values: "+ values/5);
