14efa18af4358d4cb6f6ad493fec8272fb540d57
gautamb97 <www.gautam971997@gmail.com>
2021-03-23 21:10:02 +0530

    Added:Feet to meter conversion

--- SequencePracticeProblems/SequenceProblems.js
+++ SequencePracticeProblems/SequenceProblems.js
@@ -29,3 +29,10 @@ let oneFoot = 12; // one foot is equals to 12 inch
 let inch = 42;
 let inchToFootConversion = inch/oneFoot;
 console.log("42 inches is equals to: "+inchToFootConversion+" ft");
+
+//Rectangular Plot of 60 feet x 40 feet in meters
+let lengthOfPlot = 60;
+let breadthOfPlot = 40;
+let oneMeter = 3.28; //one meter is equals to 3.2 feet
+let areaOfPlotInMeters = lengthOfPlot * breadthOfPlot / oneMeter;
+console.log("Area of rectangular plot is: "+areaOfPlotInMeters+" m");
