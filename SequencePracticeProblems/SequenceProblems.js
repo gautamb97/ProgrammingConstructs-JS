a41277ae06db339245e8eaf64d8809ff8051e0db
gautamb97 <www.gautam971997@gmail.com>
2021-03-23 20:17:03 +0530

    Added:Generated dice values between 1-6

--- SequencePracticeProblems/SequenceProblems.js
+++ SequencePracticeProblems/SequenceProblems.js
@@ -1,3 +1,7 @@
 //generating single random value
 let singleRandomValue = Math.floor(Math.random() * 10);
 console.log(singleRandomValue);
+
+//generating dice values between 1-6
+let diceRandomValue = 1 + (Math.floor(Math.random() * 10) % 6);
+console.log(diceRandomValue);
